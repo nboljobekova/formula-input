3b5155aaf40c79a4f82782a228aab738382f8143
n.boljobekova <nboljobekova@gmail.com>
2025-04-28 00:42:02 +0600

    form bugs are fixed

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, FormEvent } from "react";
 import { useGetSuggestionsList } from "@/hooks/useGetList";
 import { InputValue } from "@/types/inputs";
 import Autosuggest, { SuggestionsFetchRequestedParams, ChangeEvent, InputProps } from 'react-autosuggest';
@@ -16,17 +16,18 @@ export function Form() {
     }
   }, [data])
 
-  const getSuggestions = useCallback((value="") => {
+  const getSuggestions = useCallback((value: string) => {
     const inputValue = value.trim().toLowerCase();
     const inputLength = inputValue.length;
     
     let filteredData = suggestions.filter((item: InputValue) =>item.name.toLowerCase().includes(value));
     let resultData = inputLength === 0 ? [] : filteredData
-
-    setSuggestions(resultData);
+    setSuggestions(resultData)
+    return resultData;
   }, [value, suggestions])
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>, { newValue }: ChangeEvent) => {
+  const onChange = (event: FormEvent<HTMLElement>, params: ChangeEvent) => {
+    const {newValue} = params;
     setValue(newValue)
   };
 
@@ -43,7 +44,7 @@ export function Form() {
   };
 
   const onSuggestionsFetchRequested = ({ value }: SuggestionsFetchRequestedParams) => {
-    let values = getSuggestions(value);
+    let values: InputValue[] = getSuggestions(value);
     setSuggestions(values);
   };
 
