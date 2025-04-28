7062cf19a611ad40a25c45feae641fc88085fd08
n.boljobekova <nboljobekova@gmail.com>
2025-04-28 15:54:36 +0600

    style sass is added

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,31 +3,31 @@ import { useEffect, useState, useCallback, FormEvent } from "react";
 import { useGetSuggestionsList } from "@/hooks/useGetList";
 import { InputValue } from "@/types/inputs";
 import Autosuggest, { SuggestionsFetchRequestedParams, ChangeEvent, InputProps } from 'react-autosuggest';
+import styles from "./form.module.scss"
 
 
 export function Form() {
   const { data, isLoading } = useGetSuggestionsList();
   const [value, setValue] = useState("");
-  const [suggestions, setSuggestions] = useState<InputValue[]|[]>([]);
+  const [suggestions, setSuggestions] = useState<InputValue[] | []>([]);
 
   useEffect(() => {
-    if(data) {
+    if (data) {
       setSuggestions(data)
     }
   }, [data])
 
-  const getSuggestions = useCallback((value: string) => {
+  const getSuggestions = useCallback((value: string): InputValue[] => {
     const inputValue = value.trim().toLowerCase();
     const inputLength = inputValue.length;
-    
-    let filteredData = suggestions.filter((item: InputValue) =>item.name.toLowerCase().includes(value));
+
+    let filteredData = suggestions.filter((item: InputValue) => item.name.toLowerCase().includes(value));
     let resultData = inputLength === 0 ? [] : filteredData
     setSuggestions(resultData)
     return resultData;
   }, [value, suggestions])
 
-  const onChange = (event: FormEvent<HTMLElement>, params: ChangeEvent) => {
-    const {newValue} = params;
+  const onChange = (event: FormEvent<HTMLElement>, { newValue }: ChangeEvent) => {
     setValue(newValue)
   };
 
@@ -43,24 +43,25 @@ export function Form() {
     setSuggestions([])
   };
 
-  const onSuggestionsFetchRequested = ({ value }: SuggestionsFetchRequestedParams) => {
+  const onSuggestionsFetchRequested = (params: SuggestionsFetchRequestedParams) => {
+    const { value } = params;
     let values: InputValue[] = getSuggestions(value);
     setSuggestions(values);
   };
 
   const inputProps: InputProps<InputValue> = {
-    placeholder: 'Choose option',
+    placeholder: 'Enter variable name',
     value,
     onChange: onChange
   };
 
-  if(isLoading) {
+  if (isLoading) {
     return <div>...Loading</div>
   }
 
   return (
-    <form className="w-100 flex flex-row items-center p-7">
-      <label className="p-7">New Variable</label>
+    <form className={`${styles.form} flex flex-row p-7`}>
+      <label>New Variable</label>
       <Autosuggest
         suggestions={suggestions}
         onSuggestionsFetchRequested={onSuggestionsFetchRequested}
